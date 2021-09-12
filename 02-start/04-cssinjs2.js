396be2113bd45db13a3c7046e1e1215cd7fcd5c6
jane <choiwodls@naver.com>
2021-09-12 23:19:19 +0900

    add js-css

--- 02-start/04-cssinjs2.js
+++ 02-start/04-cssinjs2.js
@@ -6,14 +6,25 @@ const title = document.querySelector("div.hello:first-child h1");
     title을 클릭하면 클래스 이름을 바꿔줌 - 클래스 이름에 다른 색이 적용되어있음
     더 간결한 JS 코드로 같은 효과를 얻을 수 있음
     하지만, h1 태그에 또 다른 클래스명이 있으면 해당 클래스명이 없어질 수 있음
+    function handleTitleClick() {
+        const clickedClass = "clicked";
+        if (title.className === clickedClass) {
+            title.className = "";
+        } else {
+            title.className = clickedClass;
+        }
+    }
+    function handleTitleClick() {
+        const clickedClass = "clicked";
+        if (title.classList.contains(clickedClass)) {
+            title.classList.remove(clickedClass);
+        } else {
+            title.classList.add(clickedClass);
+        }
+    }
     */
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (title.className === clickedClass) {
-    title.className = "";
-  } else {
-    title.className = clickedClass;
-  }
+  title.classList.toggle("clicked");
 }
 
 title.addEventListener("click", handleTitleClick);
