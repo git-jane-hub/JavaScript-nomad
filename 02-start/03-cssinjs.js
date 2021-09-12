21260988ebe91533f21a146883674c23e4490130
jane <choiwodls@naver.com>
2021-09-12 22:51:50 +0900

    js-css

--- 02-start/03-cssinjs.js
+++ 02-start/03-cssinjs.js
@@ -1,20 +1,24 @@
 const title = document.querySelector("div.hello:first-child h1");
 
-// click 하면 두가지 색이 번갈아가면서 변경되게
+// click 하면 두가지 색이 번갈아가면서 변경되게 작성
 /*
-    curColor와 newColor를 각각 변수로 지정해야하는 이유
-    : 
+  curColor와 newColor 모두 title의 색을 나타냄
+  - title의 색(curColor)이 royalblue일 때, 
+    newColor 는 thistle로 변경
+  - title의 색(curColor)이 royalblue가 아닐 때, 
+    newColor 는 royalblue로 변경
+  - title의 색에 newColor 대입
 */
 function handleTitleClick() {
   const curColor = title.style.color;
-  let newCoolor;
+  let newColor;
   console.log(curColor);
   if (curColor === "royalblue") {
-    newCoolor = "thistle";
+    newColor = "thistle";
   } else {
-    newCoolor = "royalblue";
+    newColor = "royalblue";
   }
-  title.style.color = newCoolor;
+  title.style.color = newColor;
 }
 
 title.addEventListener("click", handleTitleClick);
---
+++ 02-start/04-cssinjs2.js
@@ -0,0 +1,19 @@
+const title = document.querySelector("div.hello:first-child h1");
+
+/*
+    03-cssinjs.js에서 변수를 지정해 
+    title을 클릭하면 해당 변수에 대입된 색이 변경된 것과 다르게
+    title을 클릭하면 클래스 이름을 바꿔줌 - 클래스 이름에 다른 색이 적용되어있음
+    더 간결한 JS 코드로 같은 효과를 얻을 수 있음
+    하지만, h1 태그에 또 다른 클래스명이 있으면 해당 클래스명이 없어질 수 있음
+    */
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  if (title.className === clickedClass) {
+    title.className = "";
+  } else {
+    title.className = clickedClass;
+  }
+}
+
+title.addEventListener("click", handleTitleClick);
