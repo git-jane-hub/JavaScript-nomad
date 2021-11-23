fdf4228bfb9de324e6df3707ab411165a84be155
jane <choiwodls@naver.com>
2021-11-23 23:16:34 +0900

    preventdefault

--- 03-login/app.js
+++ 03-login/app.js
@@ -1,9 +1,15 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+//const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
 
-function onLoginBtnClick() {
+// 해당 함수가 호출되면 브라우저가 event로 어떠한 정보를 넘겨주는지 확인
+// EventListener의 첫번째 매개변수는 항상 방금 일어난 submit event에 대한 정보
+function onLoginSubmit(event) {
+  // preventDefault(): 브라우저의 기본 동작을 막음
+  event.preventDefault();
   // loginInput 내부에 입력되는 텍스트를 변수에 저장
-  const username = loginInput.value;
+  //const username = loginInput.value;
+  console.log(info);
   /*
   if (username === "") {
     alert("Please write your name.");
@@ -17,4 +23,6 @@ function onLoginBtnClick() {
   */
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+//loginButton.addEventListener("click", onLoginBtnClick);
+// click event가 아닌 submit을 감지(enter 혹은 button을 누르면 감지)
+loginForm.addEventListener("submit", onLoginSubmit);
